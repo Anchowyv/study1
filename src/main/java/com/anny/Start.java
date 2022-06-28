bca8eaa5c629e413be7aa7c7e43d2ad0533386e1
Ilya Gonchar <iliagon1997@gmail.com>
2022-06-28 11:57:22 +0400

    first commit

---
+++ src/main/java/com/anny/Start.java
@@ -0,0 +1,15 @@
+package com.anny;
+
+public class Start {
+    public static void main(String[] args) {
+        int nun1 = 3;
+        int nun2 = 4;
+
+
+        int result = nun1 + nun2;
+
+        System.out.println(result);
+
+    }
+}
+
