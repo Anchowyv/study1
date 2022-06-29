e7d13a9ee8cc95368fc147424c41aaf3fd009b9e
Anchowyv <anys27944@gmail.com>
2022-06-29 12:05:10 +0400

    commit

--- src/main/java/com/anny/Start.java
+++ src/main/java/com/anny/Start.java
@@ -2,14 +2,20 @@ package com.anny;
 
 public class Start {
     public static void main(String[] args) {
-        int nun1 = 3;
-        int nun2 = 4;
+        int nun1 = 7;
+        int nun2 = 6;
+        int nun3 = nun2;
+
+        int result = nun1 % nun2;
 
 
-        int result = nun1 + nun2;
+        if (nun1 != nun2) {
 
-        System.out.println(result);
-
+            System.out.println("Результат равен:");
+        } else {
+            System.out.println("Результат не равен :");
+        }
     }
 }
+
 
